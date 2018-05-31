4d7a30b66d834a58b1618ed3fa6caaa15b08c529
karam <karam@arab-creative.com>
2018-05-31 14:59:56 +0300

    starting by phone transaction

---
+++ app/src/main/java/com/pos/leaders/leaderspossystem/Pinpad/Credix/TransactionCode.java
@@ -0,0 +1,11 @@
+package com.pos.leaders.leaderspossystem.Pinpad.Credix;
+
+/**
+ * Created by KARAM on 31/05/2018.
+ */
+
+public class TransactionCode {
+    public static final String REGULAR_TRANSACTION = "REGULAR_TRANSACTION";
+    public static final String PHONE_TRANSACTION = "PHONE_TRANSACTION";
+    public static final String SIGNATURE_ONLY_TRANSACTION = "SIGNATURE_ONLY_TRANSACTION";
+}
