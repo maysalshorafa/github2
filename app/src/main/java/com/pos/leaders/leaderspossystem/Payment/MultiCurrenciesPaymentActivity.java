7f0d731df4062ba1dc302d762c17797914e371d1
karam <karam@arab-creative.com>
2018-07-02 12:52:53 +0300

    baseline

---
+++ app/src/main/java/com/pos/leaders/leaderspossystem/Payment/MultiCurrenciesPaymentActivity.java
@@ -0,0 +1,15 @@
+package com.pos.leaders.leaderspossystem.Payment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pos.leaders.leaderspossystem.R;
+
+public class MultiCurrenciesPaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_currencies_payment);
+    }
+}
