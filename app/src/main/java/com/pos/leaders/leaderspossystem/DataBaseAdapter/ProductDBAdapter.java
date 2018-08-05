af3a59c18c658221faaed321ed67552ae263b86a
Karam Jabareen - laptop work <karam@arab-creative.com>
2018-08-05 17:39:36 +0300

    fix product status default value

--- app/src/main/java/com/pos/leaders/leaderspossystem/DataBaseAdapter/ProductDBAdapter.java
+++ app/src/main/java/com/pos/leaders/leaderspossystem/DataBaseAdapter/ProductDBAdapter.java
@@ -65,12 +65,12 @@ public class ProductDBAdapter {
             "`" + PRODUCTS_COLUMN_STOCK_QUANTITY + "` INTEGER , `" + PRODUCTS_COLUMN_MANAGE_STOCK + "` INTEGER NOT NULL DEFAULT 1, `" + PRODUCTS_COLUMN_IN_STOCK + "` INTEGER NOT NULL DEFAULT 1, " +
             "`" + PRODUCTS_COLUMN_WEIGHABLE + "` INTEGER NOT NULL DEFAULT 0, `" + PRODUCTS_COLUMN_CREATINGDATE + "` TIMESTAMP NOT NULL DEFAULT current_timestamp, " +
             "`" + PRODUCTS_COLUMN_DISENABLED + "` INTEGER DEFAULT 0, `" + PRODUCTS_COLUMN_CATEGORYID + "` INTEGER NOT NULL, " +
-            "`" + PRODUCTS_COLUMN_BYUSER + "` INTEGER NOT NULL, `" + PRODUCTS_COLUMN_STATUS + "` INTEGER NOT NULL DEFAULT 0 , " +
+            "`" + PRODUCTS_COLUMN_BYUSER + "` INTEGER NOT NULL, `" + PRODUCTS_COLUMN_STATUS + "` TEXT NOT NULL DEFAULT 'PUBLISHED' , " +
             "`" + PRODUCTS_COLUMN_with_pos + "` INTEGER NOT NULL DEFAULT 1, `" + PRODUCTS_COLUMN_with_point_system + "` INTEGER NOT NULL DEFAULT 1 )";
 
-    public static final String DATABASE_UPDATE_FROM_V1_TO_V2 = "alter table products rename to product_v1;" + DATABASE_CREATE + "; " +
+    public static final String DATABASE_UPDATE_FROM_V1_TO_V2[] = {"alter table products rename to product_v1;", DATABASE_CREATE + "; ",
             "insert into products (id,displayName,barcode,description,price,costPrice,regularPrice,withTax,weighable,creatingDate,hide,categoryId,byEmployee,with_pos,with_point_system) " +
-            "select id,name,barcode,description,price,costPrice,price,withTax,weighable,creatingDate,hide,depId,byEmployee,with_pos,with_point_system from product_v1;";
+                    "select id,name,barcode,description,price,costPrice,price,withTax,weighable,creatingDate,hide,depId,byEmployee,with_pos,with_point_system from product_v1;"};
 
     // Variable to hold the database instance
     public SQLiteDatabase db;
